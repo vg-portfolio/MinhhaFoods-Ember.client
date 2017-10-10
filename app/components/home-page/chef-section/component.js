8bc8c8a3be61be411c90330b59145bd27fac25bb
V4Vince <vince.guoo@gmail.com>
2017-10-10 02:54:25 -0400

    Completes default load feature for product and dis

--- app/components/home-page/chef-section/component.js
+++ app/components/home-page/chef-section/component.js
@@ -4,23 +4,39 @@ export default Ember.Component.extend({
   chefCat: [],
   selectedDishes: [],
 
+  pageSetup: function(){
+    this.loadCategories();
+    this.defaultDishes();
+  }.on('init'),
+
   loadCategories: function(){
-    this.get('categories').forEach((item) => {
+     this.get('categories').forEach((item) => {
       if (item.data.categorizableType === 'ChefSection') {
         this.get('chefCat').pushObject(item);
       }
     });
-  }.on('init'),
+  },
+
+  defaultDishes: function(){
+    let categories = this.get('chefCat');
+    console.log(categories);
+    let defaultCategory = categories.get('firstObject');
+    this.loadDishes(defaultCategory);
+  },
+
+  loadDishes: function(category){
+    this.get('selectedDishes').clear();
+    category.get('dishes')
+    .then((dishes) => {
+      dishes.forEach((dish) => {
+        this.get('selectedDishes').pushObject(dish);
+      });
+    });
+  },
 
   actions: {
     showDishes(category){
-      this.get('selectedDishes').clear();
-      category.get('dishes')
-      .then((dishes) => {
-        dishes.forEach((dish) => {
-          this.get('selectedDishes').pushObject(dish);
-        });
-      });
+      this.loadDishes(category);
     }//showDishes
   }
 });
--- app/components/home-page/product-section/component.js
+++ app/components/home-page/product-section/component.js
@@ -4,23 +4,42 @@ export default Ember.Component.extend({
   productCat: [],
   selectedProducts: [],
 
+//Sets up page by loadings array of categories and products
+pageSetup:function(){
+  this.loadCategories();
+  this.defaultProducts();
+}.on('init'),
+
+//Loads an array of categories on load
   loadCategories: function(){
     this.get('categories').forEach((item) => {
       if (item.data.categorizableType === 'ProductSection') {
         this.get('productCat').pushObject(item);
       }
     });
-  }.on('init'),
+  },
+
+//Loads an array of products on load
+  defaultProducts: function(){
+    let categories = this.get('productCat');
+    let defaultCategory = categories.get('firstObject');
+    this.loadProducts(defaultCategory);
+  },
+
+//Logic that populates product array to be loaded
+  loadProducts: function(category){
+    this.get('selectedProducts').clear();
+    category.get('products')
+    .then((products) => {
+      products.forEach((product) => {
+        this.get('selectedProducts').pushObject(product);
+      });
+    });
+  },
 
   actions: {
     showProducts(category){
-      this.get('selectedProducts').clear();
-      category.get('products')
-      .then((products) => {
-        products.forEach((product) => {
-          this.get('selectedProducts').pushObject(product);
-        });
-      });
+      this.loadProducts(category);
     }//showProducts
   }
 });
