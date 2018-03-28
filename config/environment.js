31e07271f2121efc17f8af4d99a9f856478e7a99
V4Vince <vince.guoo@gmail.com>
2018-03-28 18:02:17 -0400

    Allows external redirecting facebook url

--- config/environment.js
+++ config/environment.js
@@ -64,7 +64,7 @@ module.exports = function (environment) {
 
   if (environment === 'production') {
     ENV.rootURL = '/';
-    ENV.locationType = 'hash';
+    ENV.locationType = 'auto';
     ENV.apiHost = 'https://minha-api.herokuapp.com';
   }
 
